b2638117bca6faebd93e0c0869d74e0869627f30
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-02-10 20:58:22 -0300

    add middleware para verificar planos gratuitos e pro

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const {
+	v4: uuidv4
+} = require('uuid');
 
 const app = express();
 
@@ -7,16 +9,15 @@ app.use(express.json());
 
 
 const users = [];
-console.log(users);
 
 function checksExistsUserAccount(req, res, next) {
-
 	const { username } = req.headers;
-
 	const user = users.find((users) => users.username === username);
 
-	if(!user){
-		return res.status(400).json({error: 'user not found'})
+	if (!user) {
+		return res.status(400).json({
+			error: 'user not found'
+		})
 	}
 
 	req.user = user;
@@ -24,55 +25,90 @@ function checksExistsUserAccount(req, res, next) {
 	return next();
 }
 
+function checksCreateTodosUserAvailability(req, res, next) {
+	const { username } = req.headers;
+
+	const user = users.find((users) => users.username === username);
+	if ((!user.hasProPlan && user.todos.length < 10) || (user.hasProPlan)) {
+		return next();
+	} else {
+		return res.json({
+			message: "O usuário não pode mais criar tarefas."
+		})
+	}
+}
+
 app.post('/users', (req, res) => {
-    const { name, username} = req.body;
+	const { name, username } = req.body;
 
-    const userAlreadyExist = users.find((users) =>
-        users.username === username);
+	const userAlreadyExist = users.find((users) =>
+		users.username === username);
 
-    if (userAlreadyExist) {
-        return res.status(400).json({ error: "Usuário já existe!"})
-    }
-    const user = {
-        name,
-        username,
-        id: uuidv4(),
+	if (userAlreadyExist) {
+		return res.status(400).json({
+			error: "Usuário já existe!"
+		})
+	}
+	const user = {
+		name,
+		username,
+		hasProPlan: false,
+		id: uuidv4(),
 		todos: []
-    };
+	};
 
 	users.push(user);
 
-    return res.status(201).send();
+	return res.status(201).send();
 
+});
+
+app.get('/user', checksExistsUserAccount, (req, res) => {
+	const {
+		user
+	} = req;
+	return res.json(user);
+});
+
+
+app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (req, res) => {
+	const { user } = req;
+	const {
+		title,
+		deadline
+	} = req.body;
+
+	const todo = {
+		id: uuidv4(),
+		title,
+		done: false,
+		deadline: new Date(deadline),
+		created_at: new Date()
+	};
+
+	user.todos.push(todo)
+
+	return res.status(201).send(todo);
 });
 
 app.get('/todos', checksExistsUserAccount, (req, res) => {
-    const { user } = req;
+	const {
+		user
+	} = req;
 
 	return res.json(user.todos);
 });
-
-app.post('/todos', checksExistsUserAccount, (req, res) => {
-    const { user } = req;
-	const { title, deadline } = req.body;
-
-		const todo = {
-			id: uuidv4(),
-			title,
-			done: false,
-			deadline: new Date(deadline),
-			created_at: new Date()
-		};
-
-		user.todos.push(todo)
-
-		return res.status(201).send(todo);
-});
-
 app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
-	const { user } = req;
-	const { title, deadline } = req.body;
-	const { id } = req.params;
+	const {
+		user
+	} = req;
+	const {
+		title,
+		deadline
+	} = req.body;
+	const {
+		id
+	} = req.params;
 
 	const todo = user.todos.find((todo) => todo.id === id);
 
@@ -83,9 +119,15 @@ app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
-	const { user } = req;
-	const { done } = req.body;
-	const { id } = req.params;
+	const {
+		user
+	} = req;
+	const {
+		done
+	} = req.body;
+	const {
+		id
+	} = req.params;
 
 	const todo = user.todos.find((todo) => todo.id === id);
 	console.log(user)
@@ -96,15 +138,23 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
-	const { user } = req;
-	const { id } = req.params;
+	const {
+		user
+	} = req;
+	const {
+		id
+	} = req.params;
 
 	const todo = user.todos.find((todo) => todo.id === id);
-	try{
+	try {
 		todo.delete()
-		return res.status(200).send({message: "Excluído com sucesso"})
+		return res.status(200).send({
+			message: "Excluído com sucesso"
+		})
 	} catch {
-		return res.status(400).send({error: "Falha ao excluir"})
+		return res.status(400).send({
+			error: "Falha ao excluir"
+		})
 	}
 });
 
